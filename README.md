3ed45060daefbbee960810afb03ab2a3aaccc5cb
James Hales <james@jameshales.org>
2023-01-28 17:54:07 +1100

    Add project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Name-a-Gobbo
+
+A random name generator for goblins.
+
+## Usage
+
+[Try it online](http://jameshales.github.io/name-a-gobbo).
+
+Happy Goblin Week 2023!
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ js/main.js
@@ -0,0 +1,16 @@
+const grammar = tracery.createGrammar(nameGrammar);
+grammar.addModifiers(baseEngModifiers);
+
+function generateNames() {
+  const nodes = []
+  for (let i = 0; i < 20; i++) {
+    const name = grammar.flatten("#name#")
+    nodes.push($("<li></li>").text(name))
+  }
+  console.log(nodes)
+  $(".card ul").empty().append(nodes)
+}
+
+$(document).ready(function() {
+  generateNames()
+})
